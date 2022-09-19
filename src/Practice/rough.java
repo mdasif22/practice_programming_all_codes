911463b198392d1a219b7b58756572cd7f5ad60f
Md Asif <imd.asif222@gmail.com>
2022-09-19 23:12:28 +0530

    test

--- src/Practice/rough.java
+++ src/Practice/rough.java
@@ -8,8 +8,11 @@ public class rough {
 	public static void main(String[] args) {
 		
 		int[] nums = {2,0,2,1,1,0};
+		// Adding comments
 		
 		System.out.println(Arrays.toString(nums));
 	}
 
 }
+
+
